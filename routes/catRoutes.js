a36588b7eeafc32a4dcfa5f91c6b2d87c1e4fb69
Elizabeth <tjeudy.e@gmail.com>
2023-07-04 22:44:35 -0400

    implemented routes

--- routes/catRoutes.js
+++ routes/catRoutes.js
@@ -2,13 +2,16 @@ const express = require("express");
 const catController = require("../controllers/catController");
 const router = express.Router();
 
+// routes to homepage, displaying inputs
 router.route("/").get(catController.getAllCats);
 
+// routes to upload page
 router
 	.route("/upload")
 	.get(catController.uploadPage)
 	.post(catController.upload.single("image"), catController.createCat);
 
+// routes to edit page
 router
 	.route("/edit/:id")
 	.get(catController.editPage)
