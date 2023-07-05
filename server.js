f0d8d80bb49d9a09caa07cb36b70293ebaebf7c5
Elizabeth <tjeudy.e@gmail.com>
2023-07-05 10:48:05 -0400

    gitignore

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 require(`dotenv`).config();
 const express = require(`express`);
+const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3500;
-const mongoose = require("mongoose");
 const connectDB = require("./config/connectDB");
+// const catRoutes = require("./routes/catRoutes");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+// app.use("/", catRoutes);
 
 mongoose.connection.once("open", () => {
 	console.log("Connected to MongoDB");
